45d3719364ac8d2809d02ad059ae41ee36585632
Jesse Hixson <hixsonj@gmail.com>
2014-06-13 18:43:34 -0400

    Initial Commit

---
+++ DiceRoller/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  DiceRoller
+//
+//  Created by Jesse Hixson on 6/13/14.
+//  Copyright (c) 2014 Jesse Hixson. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
